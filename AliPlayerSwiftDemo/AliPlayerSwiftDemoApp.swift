662b8eba454384edb622c41f5a42c27e72468523
gzlboy <332446332@qq.com>
2024-03-21 12:13:15 +0800

    Initial Commit

---
+++ AliPlayerSwiftDemo/AliPlayerSwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliPlayerSwiftDemoApp.swift
+//  AliPlayerSwiftDemo
+//
+//  Created by admin on 2024/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AliPlayerSwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
